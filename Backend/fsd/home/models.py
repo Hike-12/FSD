36600ca81bcd0025f1c88c437f88832850b0469c
Hike-12 <160257872+Hike-12@users.noreply.github.com>
2025-03-28 11:44:38 +0530

    made hosts table in table

--- Backend/fsd/home/models.py
+++ Backend/fsd/home/models.py
@@ -6,7 +6,7 @@ from django.core.validators import MinValueValidator, MaxValueValidator, RegexVa
 class CustomUser(AbstractUser):
     ROLES = (
         ('STUDENT', 'Student'),
-        ('ADMIN', 'Admin'),
+        ('HOST', 'Host'),
         ('MENTOR', 'Mentor'),
         ('MANAGEMENT', 'Management')
     )
@@ -478,3 +478,15 @@ class ProjectSubmission(models.Model):
     
     def __str__(self):
         return f"{self.title} by {self.team.name}"
+    
+    
+
+
+class Host(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    contact_number = models.CharField(max_length=15, blank=True, null=True)
+
+    def __str__(self):
+        return self.full_name
