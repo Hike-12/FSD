05c17a2b975b9bfa4d5c80788bf2c754a459da54
Hike-12 <160257872+Hike-12@users.noreply.github.com>
2025-04-15 13:43:17 +0530

    kuch toh error aa raha tha fix kardiya

--- Frontend/fsd/src/components/Hosts/HostCompetitions.jsx
+++ Frontend/fsd/src/components/Hosts/HostCompetitions.jsx
@@ -220,7 +220,7 @@ const HostCompetitions = () => {
               >
                 <ChevronLeft className="h-6 w-6" />
               </button>
-              <h3 className="text-xl font-medium text-white bg-gradient-to-r from-teal-400 to-blue-400 bg-clip-text text-transparent">
+              <h3 className="text-xl font-medium font-white bg-gradient-to-r from-teal-400 to-blue-400 bg-clip-text text-transparent">
                 {monthNames[currentMonth.getMonth()]} {currentMonth.getFullYear()}
               </h3>
               <button 
